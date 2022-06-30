2ca6927ab6ccbdbe9fd9630006a8d21a605cd35a
farhan <fcyare4study@gmail.com>
2022-06-30 15:01:14 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/*
+* file: 5-printf.c
+* Author: farhan A mohamud
+*/
+#include <stdio.h>
+/**
+* main - prints "with proper grammar, but the outcome is a piece of art,, followed by a new line.
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art, \n");
+return (0);
+}
