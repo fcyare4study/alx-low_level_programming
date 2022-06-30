8e4388be83b86679730cb8bb249695247459ff60
farhan <fcyare4study@gmail.com>
2022-06-30 14:40:59 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 /*
- * file: 4-puts.c
- * Auth: farhan A mohamud
- */
+* file: 4-puts.c
+* Auth: farhan A mohamud
+*/
 #include <stdio.h>
 /**
- * main - prints "Programming is like building a multilingual puzzle, 
- * followed by a new line.
- * Return: always 0.
- */
+* main - prints "Programming is like building a multilingual puzzle,
+* followed by a new line.
+* Return: always 0.
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");	
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
