727fca4653fcfc274eea9dbcf85362ecc36461cf
farhan <fcyare4study@gmail.com>
2022-07-07 13:45:22 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,7 +4,6 @@
  * print_numbers -  prints the numbers, from 0 to 9
  * followed by a new line.
  *
- * Return: Always 0.
  */
 
 int print_numbers(void)
@@ -13,9 +12,7 @@ int print_numbers(void)
 
 	for (num = 0; num <= 9; num++)
 
-	_putchar((num % 10) + '0');
+	_putchar(num + '0');
 
 	_putchar('\n');
-
-	return (0);
 }
