198913ce781ff76b7f5c729d38bee80322a92df4
farhan <fcyare4study@gmail.com>
2022-07-11 15:50:27 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,18 +10,21 @@
 void puts_half(char *str)
 {
 	int i = 0, j;
+
 	for (; str[i] != '\0'; i++)
 	;
 
 	if (i % 2)
 	{
 		j = (i - 1) / 2;
+
 		for (; j < (i - 1); j++)
 			_putchar(str[j + 1]);
 	}
 	else
 	{
 		j = i / 2;
+
 		for (; j < i; j++)
 			_putchar(str[j]);
 	}
