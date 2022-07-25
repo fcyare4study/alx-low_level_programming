e8593b1de4c89cc0ccf298e103fff3b3f7647996
farhan <fcyare4study@gmail.com>
2022-07-25 10:54:01 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,15 +10,8 @@
  * Return; Always 0 (Success)
  */
 
-/**
- * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
- *
- * return: Always 0 (Success)int check_num(char *str)
- */
-
 int check_num(char *str)
+
 {
 /*Declaring variables*/
 unsigned int count;
@@ -57,7 +50,7 @@ int sum = 0;
 count = 1;
 while (count < argc) /*Goes through the whole array*/
 {
-if (check_num(argv[count]))
+if(check_num(argv[count]))
 
 {
 str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
