6457a654c0ea2904ae6013cae044c700c420da4d
farhan <fcyare4study@gmail.com>
2022-07-25 11:05:17 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,10 +4,10 @@
 #include <string.h>
 
 /**
- * check_num - check - string there are digit
+ * check_num - checks string there are digit
  * @str: array str
  *
- * Return; Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int check_num(char *str)
