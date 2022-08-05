2a53be3350e04904768c0bcb2673565302d29699
farhan <fcyare4study@gmail.com>
2022-08-05 13:31:30 -0700

    print name

--- 0x0F-function_pointers/variadic_functions.h
+++ 0x0F-function_pointers/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
@@ -7,4 +7,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 
-#endif
+#endif /* VARIADIC_FUNCTIONS_H */
